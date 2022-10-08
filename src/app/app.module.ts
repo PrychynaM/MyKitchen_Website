b672fcf8d8991dda04e9df6f4ad63ab7c65a3536
PrychynaM <makspricha96@gmail.com>
2022-10-09 02:21:55 +0300

    add routing,pages and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './parts-site/header/header.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
